8731ad101001e9c4d01c826b8cab44371dbd2e09
Erik <3salaz.dev@gmail.com>
2020-09-18 12:54:48 -0700

    fixed nav links

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,40 +1,41 @@
+/*jshint esversion: 6 */
 $(document).ready(() => {
-  console.log("hello");
+
   // materialize
   $(".sidenav").sidenav();
   $(".parallax").parallax();
+
+  //
+
+
   var ctx = $("#myChart");
   var myDoughnutChart = new Chart(ctx, {
     type: "doughnut",
     data: {
       labels: ["Small Medium Sized Businesses", "NonProfits", "Startups"],
-      datasets: [
-        {
-          label: "# of Votes",
-          data: [10, 4, 8],
-          backgroundColor: [
-            "rgb(138, 169, 204)",
-            "rgb(90, 98, 106)",
-            "rgb(221, 207, 125)",
-          ],
-          borderColor: [
-            "rgba(138, 169, 204, 1)",
-            "rgba(90, 98, 106, 1)",
-            "rgba(221, 207, 125, 1)",
-          ],
-          borderWidth: 1,
-        },
-      ],
+      datasets: [{
+        label: "# of Votes",
+        data: [10, 4, 8],
+        backgroundColor: [
+          "rgb(138, 169, 204)",
+          "rgb(90, 98, 106)",
+          "rgb(221, 207, 125)",
+        ],
+        borderColor: [
+          "rgba(138, 169, 204, 1)",
+          "rgba(90, 98, 106, 1)",
+          "rgba(221, 207, 125, 1)",
+        ],
+        borderWidth: 1,
+      }, ],
     },
     options: {
       scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true,
-            },
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
           },
-        ],
+        }, ],
       },
     },
   });
